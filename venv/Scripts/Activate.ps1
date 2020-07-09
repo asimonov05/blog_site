8c708d620e0883dbdf60583bf73f4782fb0686eb
Tritireciot <valchuk.kirya@list.ru>
2020-07-09 22:28:07 +0300

    gg
    gg

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Kirill\Desktop\blog_site\venv"
+$env:VIRTUAL_ENV="C:\Users\Kirill\Documents\GitHub\blog_site\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
