f74d0c5bdffc21056eac07705752d3e7fb3aba24
asimonov05 <asimonov05@gmail.com>
2020-07-09 22:41:24 +0300

    Base of pages

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from flask import render_template
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/themes')
+def themes():
+	return render_template('themes.html')
+
+@app.route('/subs')
+def subs():
+	return render_template('subs.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def not_found_error(error):
+    return render_template('500.html'), 500
